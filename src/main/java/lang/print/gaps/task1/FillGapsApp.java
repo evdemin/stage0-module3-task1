d705912449f7fe12b77cc16d734fdbc1d019b3c4
Evgenii Demin <evgenii.demin@aboutcontent.de>
2023-02-08 08:33:46 +0100

    Fixed code

--- src/main/java/lang/print/gaps/task1/FillGapsApp.java
+++ src/main/java/lang/print/gaps/task1/FillGapsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class FillGapsApp {
     public static void main(String[] args) {
-        System.out.println("Fixed");
+        System.out.println("fill" + "\n" + "the" + "\n" + "gaps" + "\n" + "to" + "\n" + "print" + "\n" + "'Fixed'");
     }
 }
